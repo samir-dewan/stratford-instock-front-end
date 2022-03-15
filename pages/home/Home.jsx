31c865f1c45f63bd93a9ebada9f1c4a8cb753888
Wahaj Arsal <a.wahaj@hotmail.co.uk>
2022-03-15 15:55:26 +0000

    Home Page set-up, so others can start to add their components inside

--- pages/home/Home.jsx
+++ pages/home/Home.jsx
@@ -1,1 +1,13 @@
 /** @format */
+
+//IMPORT MODULES
+import React, { Component } from "react";
+
+//IMPORT COMPONENTS
+import WarehouseAddNew from "../../components/warehouseAddNew/WarehouseAddNew";
+
+export default class Home extends Component {
+  render() {
+    return <WarehouseAddNew />;
+  }
+}
