95ffa0759fcf7dedb00ba2d8acec51712846758c
Wahaj Arsal <a.wahaj@hotmail.co.uk>
2022-03-16 14:40:45 +0000

    finished mobile and tablet add warehosue item

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 
-import WarehouseAddNew from "./warehouseAddNew/WarehouseAddNew";
+import WarehouseAddNew from "./components/Warehouse/warehouseAddNew/WarehouseAddNew";
 
 export default function App() {
   return (
---
+++ src/components/Utilities/Button/Button.jsx
@@ -0,0 +1,8 @@
+/** @format */
+
+import "./button.scss";
+
+const Button = ({ className, label }) => {
+  return <button className={className}>{label}</button>;
+};
+export default Button;
