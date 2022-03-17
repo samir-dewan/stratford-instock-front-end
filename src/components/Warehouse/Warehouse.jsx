1d8e426097766e05e74c6b0efd58a32ba66373fc
Wahaj Arsal <a.wahaj@hotmail.co.uk>
2022-03-17 22:01:49 +0000

    saving before pull

--- src/components/Warehouse/Warehouse.jsx
+++ src/components/Warehouse/Warehouse.jsx
@@ -3,34 +3,107 @@
 //IMPORT MODULES
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
+import axios from "axios";
 
 //IMPORT COMPONENTS
 import WarehouseAddNew from "./WarehouseAddNew/WarehouseAddNew";
 import WarehouseDetails from "./WarehouseDetails/WarehouseDetails";
 import WarehouseEdit from "./WarehouseEdit/WarehouseEdit";
 
+const API_URL_NEW_WAREHOUSE = `localhost:5000/warehouse/add-new`;
 export default class Warehouse extends Component {
-	render() {
-		return (
-			<>
-				<Switch>
-					<Route
-						path="/:warehouseId"
-						exact
-						render={(routerProps) => <WarehouseDetails {...routerProps} />}
-					/>
-					<Route
-						path="/:warehouseId/edit"
-						exact
-						render={(routerProps) => <WarehouseEdit {...routerProps} />}
-					/>
-					<Route
-						path="/:warehouseId/add-new"
-						exact
-						render={(routerProps) => <WarehouseAddNew {...routerProps} />}
-					/>
-				</Switch>
-			</>
-		);
-	}
+  state = {
+    warehouseName: "",
+    streetAddress: "",
+    city: "",
+    country: "",
+    contact: {
+      contactName: "",
+      position: "",
+      phoneNumber: "",
+      email: "",
+    },
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // toast.configure();
+    this.createNewWarehouse(e);
+    // this.isFormValid(e);
+  };
+
+  isFormValid = (e) => {
+    if (this.state.title.length < 3 || this.state.description.length < 5) {
+      if (this.state.title.length < 3) {
+        this.setState({ inputTitleValid: false });
+        // toast.error("Error: Insert Video Title");
+        if (this.state.description.length < 5) {
+          this.setState({ inputDescriptionValid: false });
+          // toast.error("Error: Insert Video Description");
+        } else {
+          this.setState({ inputDescriptionValid: true });
+        }
+      } else {
+        this.setState({ inputTitleValid: true });
+        // toast.error("Error: Insert a Video Description");
+        return;
+      }
+    } else {
+      this.setState({ inputTitleValid: true, inputDescriptionValid: true });
+      // toast.success("Uploading...");
+      this.uploadVideo();
+      e.target.title.value = null;
+      e.target.description.value = null;
+      this.delayTimerRedirect();
+    }
+  };
+
+  //******** API Call To Upload A Video ******** */
+  //Posts A Comment To The Video
+  createNewWarehouse = async (event) => {
+    const newWarehouse = {
+      warehouseName: this.state.warehouseName,
+      streetAddress: this.state.streetAddress,
+      city: this.state.city,
+      country: this.state.country,
+      contact: {
+        contactName: this.state.contactName,
+        position: this.state.position,
+        phoneNumber: this.state.phoneNumber,
+        email: this.state.email,
+      },
+    };
+    await axios.post(API_URL_NEW_WAREHOUSE, newWarehouse).then((response) => {
+      // this.getNewComment();
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Switch>
+          <Route
+            path="/:warehouseId"
+            exact
+            render={(routerProps) => <WarehouseDetails {...routerProps} />}
+          />
+          <Route
+            path="/:warehouseId/edit"
+            exact
+            render={(routerProps) => <WarehouseEdit {...routerProps} />}
+          />
+          <Route
+            path="/add-new"
+            exact
+            render={(routerProps) => (
+              <WarehouseAddNew
+                {...routerProps}
+                handleSubmit={this.handleSubmit}
+              />
+            )}
+          />
+        </Switch>
+      </>
+    );
+  }
 }
--- src/components/Warehouse/warehouseAddNew/WarehouseAddNew.jsx
+++ src/components/Warehouse/warehouseAddNew/WarehouseAddNew.jsx
@@ -5,7 +5,7 @@ import Button from "../../Utilities/Button/Button.jsx";
 import backArrow from "../../../assets/icons/arrow_back-24px.svg";
 import error from "../../../assets/icons/error-24px.svg";
 
-const WarehouseAddNew = () => {
+const WarehouseAddNew = ({ handleSubmit }) => {
   return (
     <section>
       <div className="warehouse">
@@ -14,99 +14,101 @@ const WarehouseAddNew = () => {
           <h1 className="warehouse__title">Add New Warehouse</h1>
         </div>
         <div className="form-container">
-          <form className="form">
-            <h2 className="form__heading">Warehouse Details</h2>
-            <label className="form__label">Warehouse Name</label>
-            <input
-              className="form__input form__input--error"
-              input="text"
-              name="name"
-              placeholder="Warehouse Name"
-            />
-            <p className="form__error form__error--active">
-              <img className="form__error-icon" src={error} />
-              This field is required
-            </p>
-            <label className="form__label">Street Address</label>
-            <input
-              className="form__input form__input--error"
-              input="text"
-              name="address"
-              placeholder="Street Address"
-            />
-            <p className="form__error form__error--active">
-              <img className="form__error-icon" src={error} />
-              This field is required
-            </p>
-            <label className="form__label ">City</label>
-            <input
-              className="form__input form__input--error"
-              input="text"
-              name="city"
-              placeholder="City"
-            />
-            <p className="form__error form__error--active">
-              <img className="form__error-icon" src={error} />
-              This field is required
-            </p>
-            <label className="form__label">Country</label>
-            <input
-              className="form__input form__input--error"
-              input="text"
-              name="Country"
-              placeholder="Country"
-            />
-            <p className="form__error form__error--active">
-              <img className="form__error-icon" src={error} />
-              This field is required
-            </p>
-          </form>
-          <form className="form">
-            <h2 className="form__heading">Contact Details</h2>
-            <label className="form__label">Contact Name</label>
-            <input
-              className="form__input form__input--error"
-              input="text"
-              name="name"
-              placeholder="Contact Name"
-            />
-            <p className="form__error form__error--active">
-              <img className="form__error-icon" src={error} />
-              This field is required
-            </p>
-            <label className="form__label">Position</label>
-            <input
-              className="form__input form__input--error"
-              input="text"
-              name="address"
-              placeholder="Position"
-            />
-            <p className="form__error form__error--active">
-              <img className="form__error-icon" src={error} />
-              This field is required
-            </p>
-            <label className="form__label">Phone Number</label>
-            <input
-              className="form__input form__input--error"
-              input="text"
-              name="city"
-              placeholder="Phone Number"
-            />
-            <p className="form__error form__error--active">
-              <img className="form__error-icon" src={error} />
-              This field is required
-            </p>
-            <label className="form__label">Email</label>
-            <input
-              className="form__input form__input--error"
-              input="text"
-              name="Country"
-              placeholder="Email"
-            />
-            <p className="form__error form__error--active">
-              <img className="form__error-icon" src={error} />
-              This field is required
-            </p>
+          <form onSubmit={handleSubmit}>
+            <div className="form">
+              <h2 className="form__heading">Warehouse Details</h2>
+              <label className="form__label">Warehouse Name</label>
+              <input
+                className="form__input form__input--error"
+                input="text"
+                name="name"
+                placeholder="Warehouse Name"
+              />
+              <p className="form__error form__error--active">
+                <img className="form__error-icon" src={error} />
+                This field is required
+              </p>
+              <label className="form__label">Street Address</label>
+              <input
+                className="form__input form__input--error"
+                input="text"
+                name="address"
+                placeholder="Street Address"
+              />
+              <p className="form__error form__error--active">
+                <img className="form__error-icon" src={error} />
+                This field is required
+              </p>
+              <label className="form__label ">City</label>
+              <input
+                className="form__input form__input--error"
+                input="text"
+                name="city"
+                placeholder="City"
+              />
+              <p className="form__error form__error--active">
+                <img className="form__error-icon" src={error} />
+                This field is required
+              </p>
+              <label className="form__label">Country</label>
+              <input
+                className="form__input form__input--error"
+                input="text"
+                name="Country"
+                placeholder="Country"
+              />
+              <p className="form__error form__error--active">
+                <img className="form__error-icon" src={error} />
+                This field is required
+              </p>
+            </div>
+            <div className="form">
+              <h2 className="form__heading">Contact Details</h2>
+              <label className="form__label">Contact Name</label>
+              <input
+                className="form__input form__input--error"
+                input="text"
+                name="name"
+                placeholder="Contact Name"
+              />
+              <p className="form__error form__error--active">
+                <img className="form__error-icon" src={error} />
+                This field is required
+              </p>
+              <label className="form__label">Position</label>
+              <input
+                className="form__input form__input--error"
+                input="text"
+                name="address"
+                placeholder="Position"
+              />
+              <p className="form__error form__error--active">
+                <img className="form__error-icon" src={error} />
+                This field is required
+              </p>
+              <label className="form__label">Phone Number</label>
+              <input
+                className="form__input form__input--error"
+                input="text"
+                name="city"
+                placeholder="Phone Number"
+              />
+              <p className="form__error form__error--active">
+                <img className="form__error-icon" src={error} />
+                This field is required
+              </p>
+              <label className="form__label">Email</label>
+              <input
+                className="form__input form__input--error"
+                input="text"
+                name="Country"
+                placeholder="Email"
+              />
+              <p className="form__error form__error--active">
+                <img className="form__error-icon" src={error} />
+                This field is required
+              </p>
+            </div>
           </form>
         </div>
         <div className="button__container">
