6715883668a44aae5c3e9d26be2e76a98ca92eb6
Dünya <53619378+Dunya-8a@users.noreply.github.com>
2022-03-17 11:15:27 +0000

    Adapted routing and added new utilities components for the List pages

--- src/components/Inventory/Inventory.jsx
+++ src/components/Inventory/Inventory.jsx
@@ -5,6 +5,9 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 
 //IMPORT COMPONENTS
+import InventoryItem from "./InventoryItem/InventoryItem"
+import InventoryEdit from "./InventoryEdit/InventoryEdit";
+import InventoryAddNew from "./InventoryAddNew/InventoryAddNew";
 
 export default class Inventory extends Component {
   render() {
