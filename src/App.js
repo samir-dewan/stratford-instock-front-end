c10626411df60c2c7b278dc6bd4e85c3256111b8
Wahaj Arsal <a.wahaj@hotmail.co.uk>
2022-03-17 09:31:16 +0000

    routing sorted into their own components

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Header from "./components/Header/Header";
 
-import WarehouseAddNew from "./components/Warehouse/warehouseAddNew/WarehouseAddNew";
+import WarehouseAddNew from "./components/Warehouse/WarehouseAddNew/WarehouseAddNew";
 
 export default function App() {
   return (
     <>
       <Header />
       <Switch>
-        {/* <Route exact path="/">
+        <Route exact path="/">
           <Redirect to="/warehouse" />
         </Route>
         <Route
@@ -19,41 +19,12 @@ export default function App() {
           exact
           render={(routerProps) => <Warehouse {...routerProps} />}
         />
+
         <Route
-          path="/warehouse/:warehouseId"
-          exact
-          render={(routerProps) => <WarehouseDetails {...routerProps} />}
-        />
-        <Route
-          path="/warehouse/:warehouseId/edit"
-          exact
-          render={(routerProps) => <WarehouseEdit {...routerProps} />}
-        /> */}
-        <Route
-          path="/warehouse/add-new"
-          exact
-          render={(routerProps) => <WarehouseAddNew {...routerProps} />}
-        />
-        {/* <Route
           path="/inventory"
           exact
           render={(routerProps) => <Inventory {...routerProps} />}
         />
-        <Route
-          path="/inventory/:itemId"
-          exact
-          render={(routerProps) => <InventoryItem {...routerProps} />}
-        />
-        <Route
-          path="/inventory/:itemId/edit"
-          exact
-          render={(routerProps) => <InventoryEdit {...routerProps} />}
-        />
-        <Route
-          path="/inventory/:itemId/add-new"
-          exact
-          render={(routerProps) => <InventoryAddNew {...routerProps} />}
-        /> */}
       </Switch>
     </>
   );
